3b3f342d964fcd74d5e3a00702c171d92c148990
Harry Heckmann <harry@Harrys-MacBook-Pro.local>
2019-01-08 14:19:33 -0600

    adds updated js file

--- practice.js
+++ practice.js
@@ -14,9 +14,11 @@
   Then alert your name using dot notation.
 */
 
-//Code here
-
-
+var me = {
+  name: "Harry",
+  age: 24
+};
+alert(me.name);
 
 ////////// PROBLEM 2 //////////
 
@@ -25,24 +27,32 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
-
+var favoriteThings = {
+  band: "Dave Mathews Band",
+  food: "Steak",
+  person: "My wife :)",
+  book: "The Lord of the Rings",
+  movie: "Return of the King",
+  holiday: "Christmas" 
+};
 
 
 /*
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Subaru";
+favoriteThings.brand = "AMD";
 
+console.log(favoriteThings);
 
 
 /*
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
-
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 
 
 ////////// PROBLEM 3 //////////
@@ -55,7 +65,11 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+var backPack = {
+}
+var item = "firstPocket";
+backPack[item] = "chapstick";
+backPack.color = "tan";
 
 
 
@@ -63,7 +77,7 @@
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+alert(backPack)
 
 
 
@@ -73,7 +87,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log(backPack);
 
 
 
@@ -96,8 +110,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
-
+user2.name = "Tyler S. McGinnis";
+user2.email = "tyler.mcginnis@devmounta.in";
 
 
 
